ddebee58980ac887447bd1b9b3d12cf2fc30ca0a
JamesDozier8203 <jamesdozier8203@gmail.com>
2023-12-21 14:18:23 -0800

    Added dependencies and middleware

--- RapidPay.API/Program.cs
+++ RapidPay.API/Program.cs
@@ -1,11 +1,28 @@
+using RapidPay.API.Business_Rules;
+using RapidPay.API.Middleware;
+using RapidPay.Domain;
+using RapidPay.Repository;
+using RapidPay.Repository.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var services = builder.Services;
 
-builder.Services.AddControllers();
+services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+//database service
+services.AddDbContext<DataContext>();
+services.AddCors();
+
+//add dependencies
+services.AddScoped<ICreditCardRepository, CreditCardRepository>();
+services.AddScoped<ITransactionRepository, TransactionRepository>();
+services.AddScoped<ICardSecurity, CardSecurity>();
+services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
@@ -15,6 +32,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// global cors policy
+app.UseCors(x => x
+    .SetIsOriginAllowed(origin => true)
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+
+// global error handler
+app.UseMiddleware<ExceptionHandler>();
 
 app.UseHttpsRedirection();
 
